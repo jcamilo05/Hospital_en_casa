a2056d16e8733bee4e5133f69c4b5b2911786349
Daniel_Arellano <danielarellanop@gmail.com>
2021-10-09 12:38:22 -0500

    fixed partidos, added remoteDb

--- TorneoFutbol.App/TorneoFutbol.App.Frontend/Pages/Partidos/ListPartidos.cshtml.cs
+++ TorneoFutbol.App/TorneoFutbol.App.Frontend/Pages/Partidos/ListPartidos.cshtml.cs
@@ -11,18 +11,18 @@ namespace TorneoFutbol.App.Frontend.Pages.Partidos
 {
     public class ListPartidosModel : PageModel
     {
-    public Equipo equipo { get; set; }
-    private readonly IRepositorioPartido _repoPartido;
-    public IEnumerable<Partido> partidos { get; set; }
-    public ListPartidosModel(IRepositorioPartido repoPartido, Equipo equipo)
-    {
-        equipo = new Equipo();
-        _repoPartido = repoPartido;
-    }
-    public void OnGet()
-    {
+        
+        private readonly IRepositorioPartido _repoPartido;
+        public IEnumerable<Partido> partidos { get; set; }
+        public ListPartidosModel(IRepositorioPartido repoPartido)
+        {
 
-        partidos = _repoPartido.GetAllPartido();
-    }
+            _repoPartido = repoPartido;
+        }
+        public void OnGet()
+        {
+
+            partidos = _repoPartido.GetAllPartido();
+        }
     }
 }
