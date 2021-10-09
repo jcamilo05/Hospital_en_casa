b5fb19896454fe68fc218d82b245c74b58dfc1f1
Daniel Arellano <danielarellanop@gmail.com>
2021-10-08 21:16:19 -0500

    Testing Partido Preview

--- TorneoFutbol.App/TorneoFutbol.App.Frontend/Pages/Partido/Index.cshtml.cs
+++ TorneoFutbol.App/TorneoFutbol.App.Frontend/Pages/Partido/Index.cshtml.cs
@@ -4,13 +4,43 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoFutbol.App.Persistencia;
+using TorneoFutbol.App.Dominio;
 
 namespace TorneoFutbol.App.Frontend.Pages.Partido
 {
     public class IndexModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioEquipo _repoEquipo;
+        private readonly IRepositorioEstadio _repoEstadio;
+        private readonly IRepositorioPartido _repoPartido;
+        public Municipio municipio { get; set; }
+        public IEnumerable<Equipo> equipos { get; set; }
+        public IEnumerable<Estadio> estadios { get; set; }
+
+        [BindProperty]
+        public string Label { get; set; }
+        public IndexModel(IRepositorioEquipo repoEquipo, IRepositorioEstadio repoEstadio)
         {
+            
+            _repoEquipo = repoEquipo;
+            _repoEstadio = repoEstadio;
         }
+        
+        
+        public void OnGet(int id)
+        {
+            //municipio = _repoMunicipio.GetMunicipio(id);
+            equipos = _repoEquipo.GetAllEquipo();
+            estadios = _repoEstadio.GetAllEstadio();
+            /**
+            if (id == Guid.Empty) {
+                Label = "Create"; 
+            } else {
+                Label = "Update";
+            }
+            */
+        }
+        
     }
 }
