8944817ff65e3271d1f081c6a70f3449b24133c8
Daniel Arellano <danielarellanop@gmail.com>
2021-10-09 20:35:13 -0500

    implemented delete feature on arbitro

--- TorneoFutbol.App/TorneoFutbol.App.Frontend/Pages/Arbitros/Delete.cshtml.cs
+++ TorneoFutbol.App/TorneoFutbol.App.Frontend/Pages/Arbitros/Delete.cshtml.cs
@@ -4,13 +4,46 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TorneoFutbol.App.Dominio;
+using TorneoFutbol.App.Persistencia;
 
 namespace TorneoFutbol.App.Frontend.Pages.Arbitros
 {
     public class DeleteModel : PageModel
     {
-        public void OnGet()
+        private readonly IRepositorioArbitro _repoArbitro;
+
+        public Arbitro arbitro { get; set; }
+
+        public DeleteModel(IRepositorioArbitro repoArbitro)
         {
+            _repoArbitro = repoArbitro;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            arbitro = _repoArbitro.GetArbitro(id);
+            if (arbitro == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Page();
+            }
+        }
+
+        public IActionResult OnPost(Arbitro arbitro)
+        {
+            if (ModelState.IsValid)
+            {
+                _repoArbitro.DeleteArbitro (arbitro.ID);
+                return RedirectToPage("List");
+            }
+            else
+            {
+                return Page();
+            }
         }
     }
 }
