5a766a8c3e7b98a8a07d4a0c17ab0c84ec47c7f9
Juan <camilo.giar@gmail.com>
2021-10-10 23:05:28 -0500

    Set .AddRazorRuntimeCompilation to see the change in the browser

--- TorneoFutbol.App/TorneoFutbol.App.Frontend/Startup.cs
+++ TorneoFutbol.App/TorneoFutbol.App.Frontend/Startup.cs
@@ -25,7 +25,7 @@ namespace TorneoFutbol.App.Frontend
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddRazorPages();
+            services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddSingleton<IRepositorioArbitro, RepositorioArbitro>();
             services.AddSingleton<IRepositorioDT, RepositorioDT>();
             services.AddSingleton<IRepositorioEquipo, RepositorioEquipo>();
