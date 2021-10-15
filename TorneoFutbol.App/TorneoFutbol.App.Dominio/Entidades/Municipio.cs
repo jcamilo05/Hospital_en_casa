5d554507ba9b4712bb5916b5b59f6491cc2a217e
Juan <camilo.giar@gmail.com>
2021-10-15 01:17:55 -0500

    Added filters and change repositorio to find

--- TorneoFutbol.App/TorneoFutbol.App.Dominio/Entidades/Municipio.cs
+++ TorneoFutbol.App/TorneoFutbol.App.Dominio/Entidades/Municipio.cs
@@ -10,11 +10,16 @@ namespace TorneoFutbol.App.Dominio
         /// Id de cada municipio 
         public int ID { get; set; }    
         ///Nombre del municipio 
-        [Required, StringLength(50)]
+        
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [StringLength(50, MinimumLength = 2, 
+        ErrorMessage="El campo {0} debe tener {1} caracteres de máximo y {2} de mínimo")]
         public string Nombre { get; set; }
 
         ///Nombre del departamento 
-        [Required, StringLength(50)]
+        [Required(ErrorMessage = "{0} es un campo obligatorio")]
+        [StringLength(50, MinimumLength = 2, 
+        ErrorMessage="El campo {0} debe tener {1} caracteres de máximo y {2} de mínimo")]
         public string Departamento { get; set; }
         public Estadio Estadio {get; set;}
     }
